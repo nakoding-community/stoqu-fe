cb2ca68d3a19dee33204f1efd10fc73276a6a524
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2022-12-17 18:58:35 +0700

    update firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,25 +3,25 @@ import 'firebase/compat/database';
 
 // Production
 let config = {
-  apiKey: 'AIzaSyC4E9iVVm-10JjUrvMSEGRVtLHkwQTIon8',
-  authDomain: 'inamawangi-59d3e.firebaseapp.com',
-  projectId: 'inamawangi-59d3e',
-  storageBucket: 'inamawangi-59d3e.appspot.com',
-  messagingSenderId: '282036610633',
-  appId: '1:282036610633:web:9753a4fe48938f89080198',
-  measurementId: 'G-FRLWBBZBB4',
+  apiKey: 'AIzaSyB9CmFxaPSMvOXGzYmlzj_bHktilpKwq5k',
+  authDomain: 'inamawangi-revamp-dev.firebaseapp.com',
+  projectId: 'inamawangi-revamp-dev',
+  storageBucket: 'inamawangi-revamp-dev.appspot.com',
+  messagingSenderId: '288480234486',
+  appId: '1:288480234486:web:16e7dfcb5f7b5769eb15d7',
+  measurementId: 'G-Y9RW7QMRNZ',
 };
 
 // Development
 if (process.env.REACT_APP_ENV !== 'prod') {
   config = {
-    apiKey: 'AIzaSyCZrt6XSYjH1ncDCQSo1n6Rz4DgiNk__tw',
-    authDomain: 'inamawangi-d1766.firebaseapp.com',
-    projectId: 'inamawangi-d1766',
-    storageBucket: 'inamawangi-d1766.appspot.com',
-    messagingSenderId: '512541535572',
-    appId: '1:512541535572:web:936057206e1b0b7202ecc1',
-    measurementId: 'G-SFXTFEVE73',
+    apiKey: 'AIzaSyB9CmFxaPSMvOXGzYmlzj_bHktilpKwq5k',
+    authDomain: 'inamawangi-revamp-dev.firebaseapp.com',
+    projectId: 'inamawangi-revamp-dev',
+    storageBucket: 'inamawangi-revamp-dev.appspot.com',
+    messagingSenderId: '288480234486',
+    appId: '1:288480234486:web:16e7dfcb5f7b5769eb15d7',
+    measurementId: 'G-Y9RW7QMRNZ',
   };
 }
 
