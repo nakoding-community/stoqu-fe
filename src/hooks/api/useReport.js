63e3cec9c6207d692a67a482f50d5a3d0fdba933
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2022-12-18 09:14:58 +0700

    update report product page

--- src/hooks/api/useReport.js
+++ src/hooks/api/useReport.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { getReportOrders } from '../../clientv2/reportClient';
+import { getReportOrderProducts, getReportOrders } from '../../clientv2/reportClient';
 
 export const useGetReportOrders = (queryParams) => {
   const fetchData = () => {
@@ -7,4 +7,12 @@ export const useGetReportOrders = (queryParams) => {
   };
 
   return useQuery(['report-orders', 'list', queryParams], fetchData);
+};
+
+export const useGetReportProducts = (queryParams) => {
+  const fetchData = () => {
+    return getReportOrderProducts(queryParams);
+  };
+
+  return useQuery(['report-products', 'list', queryParams], fetchData);
 };
