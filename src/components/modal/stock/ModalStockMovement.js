fe52e983d68d3352a666ba720a68099df688b6e5
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2022-12-28 09:01:12 +0700

    adjust modal stock movement and preparing body

--- src/components/modal/stock/ModalStockMovement.js
+++ src/components/modal/stock/ModalStockMovement.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { useConfirm } from 'material-ui-confirm';
+import { useTheme, alpha } from '@mui/material/styles';
 
-import { Stack, DialogActions, Button } from '@mui/material';
+import { Stack, DialogActions, Button, Box, Typography, IconButton } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 
 import Modal from '../Modal';
 import InfiniteCombobox from '../../combobox/InfiniteCombobox';
+import Iconify from '../../Iconify';
 
 // eslint-disable-next-line react/prop-types
 function ModalStockMovement({ open, onClose }) {
@@ -20,8 +22,20 @@ function ModalStockMovement({ open, onClose }) {
 function DialogForm({ onClose }) {
   const confirm = useConfirm();
   const queryClient = useQueryClient();
+  const theme = useTheme();
 
-  const submitModalHandler = async () => {};
+  const submitModalHandler = async () => {
+    const body = {
+      destination: {
+        rackId: '',
+      },
+      origin: {
+        productId: '',
+        rackId: '',
+        stockLookupIds: [],
+      },
+    };
+  };
 
   const confrimHandler = (e) => {
     e.preventDefault();
@@ -45,8 +59,17 @@ function DialogForm({ onClose }) {
         />
         <InfiniteCombobox
           // value={selectedProducts?.[0]?.id}
-          label="Cari Rak"
-          type="types"
+          label="Rak Origin"
+          type="racks"
+          // onChange={onChangeProductHandler}
+          required
+          // excludeIds={selectedProducts?.map(({ id }) => id)}
+          // labelText={productLabel}
+        />
+        <InfiniteCombobox
+          // value={selectedProducts?.[0]?.id}
+          label="Rak Destination"
+          type="racks"
           // onChange={onChangeProductHandler}
           required
           // excludeIds={selectedProducts?.map(({ id }) => id)}
@@ -61,15 +84,33 @@ function DialogForm({ onClose }) {
           // excludeIds={selectedProducts?.map(({ id }) => id)}
           // labelText={productLabel}
         />
-        <InfiniteCombobox
-          // value={selectedProducts?.[0]?.id}
-          label="Paket Tujuan"
-          type="types"
-          // onChange={onChangeProductHandler}
-          required
-          // excludeIds={selectedProducts?.map(({ id }) => id)}
-          // labelText={productLabel}
-        />
+        <Stack direction="row" alignItems={'center'}>
+          <Box
+            sx={{
+              width: '40px',
+              height: '40px',
+              borderRadius: '50%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginRight: '16px',
+              color: theme.palette.success.dark,
+              backgroundColor: alpha(theme.palette.success.main, 0.16),
+            }}
+          >
+            <Iconify icon="mdi:office-building-settings" sx={{ width: '20px', height: '20px' }} />
+          </Box>
+          <Stack width="100%" direction="row" alignItems={'center'} justifyContent="space-between">
+            <Box>
+              <Typography variant="body1">Lookup 1</Typography>
+            </Box>
+            <Stack flexDirection="row" alignItems={'center'}>
+              <IconButton size="small" color="error" onClick={() => null}>
+                <Iconify icon="eva:trash-2-outline" />
+              </IconButton>
+            </Stack>
+          </Stack>
+        </Stack>
       </Stack>
       <DialogActions>
         <Button variant="outlined" color="inherit" onClick={onClose}>
