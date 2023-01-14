a798bb8693d09f8d7a4a5bf49225a95916e66962
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2023-01-14 11:12:30 +0700

    fix notes

--- src/components/order-v2/data-order/fragments/Notes.js
+++ src/components/order-v2/data-order/fragments/Notes.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { TextField } from '@mui/material';
 import { shallow } from 'zustand/shallow';
-import { useLocation } from 'react-router';
 import { useCreateOrder } from '../../../../hooks/useCreateOrderV2';
 
 export const Notes = () => {
-  const location = useLocation();
-  const isCreatePage = location.pathname.includes('new');
-
-  const isReady = false;
-
   const { notes, immerSetState } = useCreateOrder(
     (state) => ({
       notes: state.payloadBody.notes,
@@ -26,39 +20,19 @@ export const Notes = () => {
 
   return (
     <>
-      {!isCreatePage ? (
-        isReady && (
-          <TextField
-            value={notes}
-            id="outlined-basic"
-            type="text"
-            label="Catatan"
-            variant="outlined"
-            size="medium"
-            sx={{ mt: '24px', width: '100%' }}
-            onChange={onChange}
-            // disabled={!isUserAbleToEdit}
-            multiline
-            minRows={3}
-            maxRows={4}
-          />
-        )
-      ) : (
-        <TextField
-          value={notes}
-          id="outlined-basic"
-          type="text"
-          label="Catatan"
-          variant="outlined"
-          size="medium"
-          sx={{ mt: '24px', width: '100%' }}
-          onChange={onChange}
-          // disabled={!isUserAbleToEdit}
-          multiline
-          minRows={3}
-          maxRows={4}
-        />
-      )}
+      <TextField
+        value={notes}
+        id="outlined-basic"
+        type="text"
+        label="Catatan"
+        variant="outlined"
+        size="medium"
+        sx={{ mt: '24px', width: '100%' }}
+        onChange={onChange}
+        multiline
+        minRows={3}
+        maxRows={4}
+      />
     </>
   );
 };
